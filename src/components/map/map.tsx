6d94597f7fc6e872d8dfabdd33ab619c709bf08d
Marco Santos <marconlsantos@yahoo.co.uk>
2023-03-22 14:12:25 +0000

    Display marker for random brewery on start.

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -2,24 +2,38 @@ import { $, component$, useOnWindow, useStylesScoped$ } from '@builder.io/qwik';
 import L from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
+import type { BreweryRepository } from '~/domain/Repositories/BreweryRepository';
+import { OpenBreweryRepository } from '~/infrastructure/repositories/OpenBreweryRepository';
 import styles from './map.css?inline';
+
+export const repository: BreweryRepository = new OpenBreweryRepository();
 
 export const Map = component$(() => {
     useStylesScoped$(styles);
 
-    useOnWindow("load", $(() => {
+    useOnWindow("load", $(async () => {
         console.log('LOADED!!');
 
-        const map = L.map('map').fitWorld();
+        const random = await repository.getRandom();
+
+        const lat = random.coordinates === undefined ? 0 : random.coordinates.Value.latitude;
+        const lng = random.coordinates === undefined ? 0 : random.coordinates.Value.longitude;
+
+        console.log(random);
+
+        const map = L.map('map', {
+            zoomControl: false,
+        }).setView({ lat, lng }, 12);
+
+        L.control.scale().addTo(map);
 
         L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        map.locate({ setView: true, maxZoom: 11 });
+        L.marker({ lat, lng }).addTo(map);
 
     }));
-
 
     return <div id='map'></div>;
 });
